aa241dc8ce05b72781af4e354807d301124fecc4
EduardoVBF <eduardo.freitas15@fatec.sp.gov.br>
2025-10-06 11:20:34 -0300

    feat: initial config

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { server } from "./server/server.js";
+
+server.listen(3333, () => {
+    console.log("****************** OIIIIIIIIIII ******************");
+    console.log("Server is running on http://localhost:3333");
+});

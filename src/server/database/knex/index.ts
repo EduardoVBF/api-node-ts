8b45863e424b27b9d105bbbd4a090270db0df968
EduardoVBF <eduardo.freitas15@fatec.sp.gov.br>
2025-10-14 17:07:31 -0300

    inicial knex

---
+++ src/server/database/knex/index.ts
@@ -0,0 +1,17 @@
+import { knex } from "knex";
+import { development, production, test } from "./Environment.js";
+
+const getEnvironment = () => {
+  switch (process.env.NODE_ENV) {
+    case "dev":
+      return development;
+    case "test":
+      return test;
+    case "production":
+      return production;
+    default:
+      return development;
+  }
+};
+
+export const Knex = knex(getEnvironment());

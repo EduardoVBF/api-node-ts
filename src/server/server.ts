f47e26e28f665ca114062f2af133420e69f76d39
EduardoVBF <eduardo.freitas15@fatec.sp.gov.br>
2025-10-20 21:06:08 -0300

    cors fixes

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,15 @@
 import "./shared/services/TranslationsYup.js";
 import { router } from "./routes/index.js";
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 
 const server = express();
 
+server.use(cors({
+  // origin: ["http://localhost:3000", "https://appname.neon.tech"],
+  origin: ["*"],
+}));
 server.use(express.json());
 server.use(router);
 
